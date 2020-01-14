86b78698c65676f41de0baf5d24bdb49e5bf8f11
Veli Bacik <velibacik60@gmail.com>
2020-01-15 02:31:19 +0300

    user post model added

--- app/server.js
+++ app/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const app         = express()
-const port        = process.env.PORT ||3000
-const loginRouter = require('./routes/loginRouter')
+const bodyParser   = require('body-parser')
+const app          = express()
+const port         = process.env.PORT ||3000
+const loginRouter2 = require('../lib/authentication/login/router/login_router')
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.use('/api',loginRouter);
+app.use('/api',loginRouter2);
 app.listen(port, () => console.log(`Example app listening on port port!`))
