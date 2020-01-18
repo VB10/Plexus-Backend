3e8a6c3c77ebde7c8912a424503b88e284b2c9fb
Veli Bacik <velibacik60@gmail.com>
2020-01-18 23:52:10 +0300

    users list added and register completed

--- app/server.js
+++ app/server.js
@@ -3,13 +3,15 @@ const bodyParser = require("body-parser");
 const mongoose   = require("mongoose");
 
 // eslint-disable-next-line no-unused-vars
-const _           = mongoose.connect("mongodb://localhost/vbapi");
-const app         = express();
-const port        = process.env.PORT || 3000;
-const loginRouter = require("../lib/authentication/login/router/login_router");
+const _              = mongoose.connect("mongodb://localhost/vbapi");
+const app            = express();
+const port           = process.env.PORT || 3000;
+const loginRouter    = require("../lib/authentication/login/router/login_router");
+const registerRouter = require("../lib/authentication/register/router/register_router");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/api", loginRouter);
+app.use("/api", registerRouter);
 app.listen(port, () => console.log(`Example app listening on port port!`));
